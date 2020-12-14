0c43c022c27bf718eea864f978c2e92d22059c43
Drvi <tomas.drvostep@gmail.com>
2020-12-14 09:34:31 +0000

    Day 11: readability tweaks

--- day11/solve.jl
+++ day11/solve.jl
@@ -11,8 +11,8 @@ end
 function _advance!(O, M)
     h, w = size(M)
     any_changed = false
-    @inbounds for i = 3:h-2
-          for j = 3:w-2
+    @inbounds for i = 2:h-1
+          for j = 2:w-1
             x = M[i, j]
             x == 0 && continue
             o = (M[i-1, j-1] == 1) + (M[i-1, j] == 1) + (M[i-1, j+1] == 1) +
@@ -36,8 +36,8 @@ end
 
 function solve1(inputs)
     h, w = size(inputs)
-    M = zeros(eltype(inputs), h + 4, w + 4)
-    M[3:h+2, 3:w+2] .= inputs
+    M = zeros(eltype(inputs), h + 2, w + 2)
+    M[2:h+1, 2:w+1] .= inputs
     O = copy(M)
     while _advance!(O, M)
         copyto!(M, O)
@@ -63,10 +63,9 @@ function _serchdirected(start, direction, inputs)
 end
 
 function create_index(inputs::Matrix{T}) where {T}
-    directions = (
-        ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
-    )
+    directions = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1))
     m, n = size(inputs)
+    cnt_available = m * n
     zero_index = let f = findfirst(==(0), inputs)
         f[1] + (f[2] - 1) * m
     end
@@ -74,26 +73,27 @@ function create_index(inputs::Matrix{T}) where {T}
     visible_neighbors = -ones(T, 8, n * m)
     @inbounds for i = 1:m
         for j = 1:n
-            linear_index = T(i + (j - 1) * m)
-            if inputs[linear_index] == 0
-                zero_index = linear_index
+            curr_index = T(i + (j - 1) * m)
+            if inputs[curr_index] == 0
+                cnt_available -= 1
+                zero_index = curr_index
                 continue
             end
-            neighbor_buffer = @view visible_neighbors[:, linear_index]
+            curr_buffer = @view visible_neighbors[:, curr_index]
             for (k, d) in enumerate(directions)
-                neighbor_buffer[k] != -1 && continue
-                other_linear_index = _serchdirected((i, j), d, inputs)
-                if !isnothing(other_linear_index)
-                    neighbor_buffer[k] = T(other_linear_index)
-                    other_buffer = @view visible_neighbors[:, other_linear_index]
-                    other_buffer[9-k] = linear_index
+                curr_buffer[k] != -1 && continue
+                neighbor_index = _serchdirected((i, j), d, inputs)
+                if !isnothing(neighbor_index)
+                    curr_buffer[k] = T(neighbor_index)
+                    neighbor_buffer = @view visible_neighbors[:, neighbor_index]
+                    neighbor_buffer[9-k] = curr_index
                 else
-                    neighbor_buffer[k] = zero_index
+                    curr_buffer[k] = zero_index
                 end
             end
         end
     end
-    cnt_available = count(!iszero, inputs)
+
     out = Vector{SeatPosition{T}}(undef, cnt_available)
     j = 1
     @inbounds for i = 1:m*n
